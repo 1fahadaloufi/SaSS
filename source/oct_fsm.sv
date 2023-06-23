00e2ad1c8334e7567d56efc63f17b2f4c8651005
ECN Guset011 <ecng011@potr262lnx15.ecn.purdue.edu>
2023-06-23 16:31:50 -0400

    oct_fsm modifed and tb included

--- source/oct_fsm.sv
+++ source/oct_fsm.sv
@@ -1,8 +1,12 @@
 //Octave FSM module
-module nextstate_octave(input logic octave_in, input logic clk, input logic nrst, output logic [2:0]octave);
+
+module oct_fsm(input logic octave_in, input logic clk, input logic nrst,
+output [2:0] state);
 logic [2:0]next_state;
+
+
 always_comb begin
-    case(octave_in):
+    case(state)
     3'd0: next_state = (octave_in == 1) ? 1:0; //0 - O1
     3'd1: next_state = (octave_in == 1) ? 2:1; //1 - O2
     3'd2: next_state = (octave_in == 1) ? 3:2; //2 - O3
@@ -10,15 +14,17 @@ always_comb begin
     3'd4: next_state = (octave_in == 1) ? 5:4; //4 - O5
     3'd5: next_state = (octave_in == 1) ? 6:5; //5 - O6
     3'd6: next_state = (octave_in == 1) ? 0:6; //6 - O7
-    default: next_state = 3'd0;
+    default: next_state = 3'd7;
     endcase
 end
 
 always_ff @ (posedge clk, negedge nrst) begin
-    if(nrst == 1) begin
-       octave <= next_state;
-    else
-       octave <= 3'd7;
+    
+    if(!nrst) begin
+       state <= 3'd0;
+    end
+    else begin
+       state <= next_state;
     end
 end
 endmodule
