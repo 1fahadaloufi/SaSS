c9e606931ba62641ea1b17a6a06babdcb3435bfe
ECN Guset040 <ecng040@potr236lnx02.ecn.purdue.edu>
2023-06-26 15:38:28 -0400

    sequencer shenanagins

--- source/measure_counter.sv
+++ source/measure_counter.sv
@@ -1,3 +1,14 @@
+/************************************************************************************************************/
+// Inputs:
+//    clk -> 10kHz clock
+//    n_rst -> Async negative reset, resets when n_rst is 0
+//    play -> A play/pause signal which when 1 allows the measure counter to continue counting through measures and when 0 pauses the sequencer
+//    beat_pulse -> A pulse which delays the system clock signal down to the desired tempo of the sequencer, tells measure_counter when to count
+//
+// Outputs:
+//    beat -> current beat the sycnroniser is on (0-7)
+/*************************************************************************************************************/
+
 module measure_counter (input logic play, beat_pulse, clk, n_rst, output logic [2:0]beat);
 
     logic [2:0]next_beat;
