a1f7f8e15d4e6a73a37bad7d2ef7907100ed7475
ECN Guset040 <ecng040@potr236lnx02.ecn.purdue.edu>
2023-06-26 15:54:40 -0400

    changed comment on oscilator

--- source/oscilator.sv
+++ source/oscilator.sv
@@ -17,17 +17,17 @@ module oscilator (input logic clk, nrst, input logic [18:0] max, output logic [1
     if(nrst)
     count <= next_count;
     else
-    count <= 1; // Reset sets count to zero
+    count <= 1; // Reset sets count to one
   end
 
   always_comb begin
-
-    if(count == max) begin
-      next_count = 1; // Once the count hits the inputted max (divider) it wraps to 1
-    end
-    else begin
-      next_count = count + 1; // Counting by 1
-    end
+    
+      if(count == max) begin
+        next_count = 1; // Once the count hits the inputted max (divider) it wraps to 1
+      end
+      else begin
+        next_count = count + 1; // Counting by 1
+      end
 
     end 
 
