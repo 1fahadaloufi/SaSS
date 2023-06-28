c42fe164ee89990f868dfaea4f1da6713b180022
Connor Mehan <connor@mehanks.com>
2023-06-28 13:28:31 -0400

    changed wave_comb to account for seqencer

--- source/new_waveform_comb.sv
+++ source/new_waveform_comb.sv
@@ -13,10 +13,13 @@
 //    comb_waveform -> Final combined waveform value to be outputed to pwm
 /*************************************************************************************************************/
 `default_nettype none
-module new_waveform_comb (input logic [2:0]num_signals, input logic done, input logic clk, n_rst, input logic [8:0]sample1, sample2, sample3, sample4, output logic ready, output logic [8:0] comb_waveform);
+module new_waveform_comb (input logic [2:0]num_signals, input logic done, input logic clk, n_rst, seq_on, input logic [8:0]sample1, sample2, sample3, sample4, output logic ready, output logic [8:0] comb_waveform);
 
     logic [10:0] new_sample1, new_sample2, new_sample3, new_sample4, sum;
     logic [10:0] inter_waveform; // Creating new varibales with one extra bit to handle overflow when adding
+
+    logic [8:0] comb_waveform_i;
+    logic ready_i;
 
     // Inter_waveform is just an intermediate value for the waveform which stores it in 9 bits instead of 8, the final wavbeform takes the last 8 of it
 
@@ -27,6 +30,15 @@ module new_waveform_comb (input logic [2:0]num_signals, input logic done, input 
     //assign comb_waveform = inter_waveform[8:0]; // Seting output waveform to last 8 bits of the extended 9 bit intermediate waveform
 
     assign sum = new_sample1 + new_sample2 + new_sample3 + new_sample4;
-    seqdiv_comb u1 (.sample_part(sum), .voice(num_signals), .sample(done), .clk(clk), .RST(n_rst), .sample_comb(comb_waveform), .done(ready));
+    seqdiv_comb u1 (.sample_part(sum), .voice(num_signals), .sample(done), .clk(clk), .RST(n_rst), .sample_comb(comb_waveform_i), .done(ready_i));
+    always_comb begin
+        if(seq_on) begin
+            comb_waveform = sample1;
+            ready = done;
+        end
+        else
+            comb_waveform = comb_waveform_i;
+            ready = ready_i;
+    end
 
 endmodule
